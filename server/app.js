eb1cda06c023665a5ed4fc21f906c4cc7121c890
dimaiv95 <ivdim4ik95@gmail.com>
2020-08-15 21:29:58 +0300

    Init

---
+++ server/app.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+import path from "path";
+import express from "express";
+import bodyParser from "body-parser";
+import multer from "multer";
+
+const app = express();
+
+const storage = multer.diskStorage({
+    destination (req, file, cb) {
+        cb(null, path.join(__dirname, "images/"))
+    },
+    filename (req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`)
+    }
+});
+const uploadFiles = multer({ storage: storage });
+
+app.use(express.static(path.join(__dirname, "../client/public")));
+app.use("/images", express.static(path.join(__dirname, "images/")));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res, next) => {
+    res.status(200).sendFile("index.html");
+});
+
+app.listen(5000, () => {
+    console.log("Server started on 5000");
+})
